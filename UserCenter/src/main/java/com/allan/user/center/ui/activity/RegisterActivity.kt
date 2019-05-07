d13b47e25b25b771a8038127bf862eb07e4eabfa
Allansk2 <Regina@Reginas-MacBook-Air.local>
2019-05-06 19:07:28 -0600

    Config multi module

---
+++ UserCenter/src/main/java/com/allan/user/center/ui/activity/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.allan.user.center.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.allan.user.center.R
+
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+}
