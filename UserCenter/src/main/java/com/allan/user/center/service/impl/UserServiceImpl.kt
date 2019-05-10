2fbc1953128c7eec9898d9e0158095870f802549
Allansk2 <Regina@Reginas-MacBook-Air.local>
2019-05-09 20:41:57 -0600

    User okhttp client

--- UserCenter/src/main/java/com/allan/user/center/service/impl/UserServiceImpl.kt
+++ UserCenter/src/main/java/com/allan/user/center/service/impl/UserServiceImpl.kt
@@ -1,25 +1,27 @@
 package com.allan.user.center.service.impl
 
+import com.allan.base.library.data.protocol.BaseResp
+import com.allan.base.library.rx.BaseExeception
+import com.allan.user.center.data.repository.UserRepository
 import com.allan.user.center.service.UserService
 import io.reactivex.Observable
+import io.reactivex.functions.Function
 
 
 class UserServiceImpl : UserService {
     override fun register(mobile: String, verifyCode: String, pwd: String): Observable<Boolean> {
-        return Observable.just(true)
+        val repository = UserRepository()
 
-//        val repository = UserRepository()
-//
-//        return repository.register(mobile, pwd, verifyCode).flatMap(
-//            object : Function<BaseResp<String>, Observable<Boolean>> {
-//                override fun apply(t: BaseResp<String>): Observable<Boolean> {
-//                    if (t.status != 0) {
-//                        return Observable.error(BaseExeception(t.status, t.message))
-//                    }
-//
-//                    return Observable.just(true)
-//                }
-//            }
-//        )
+        return repository.register(mobile, pwd, verifyCode).flatMap(
+            object : Function<BaseResp<String>, Observable<Boolean>> {
+                override fun apply(t: BaseResp<String>): Observable<Boolean> {
+                    if (t.status != 0) {
+                        return Observable.error(BaseExeception(t.status, t.message))
+                    }
+
+                    return Observable.just(true)
+                }
+            }
+        )
     }
 }
