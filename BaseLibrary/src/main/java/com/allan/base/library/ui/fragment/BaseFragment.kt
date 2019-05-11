52cf78108eb35b74f5e56aae98e565ca775d6d60
Allansk2 <Regina@Reginas-MacBook-Air.local>
2019-05-11 09:53:51 -0600

    Add base fragment

---
+++ BaseLibrary/src/main/java/com/allan/base/library/ui/fragment/BaseFragment.kt
@@ -0,0 +1,8 @@
+package com.allan.base.library.ui.fragment
+
+import com.trello.rxlifecycle3.components.RxFragment
+
+
+open class BaseFragment : RxFragment() {
+
+}
---
+++ BaseLibrary/src/main/java/com/allan/base/library/ui/fragment/BaseMvpFragment.kt
@@ -0,0 +1,50 @@
+package com.allan.base.library.ui.fragment
+
+import android.os.Bundle
+import com.allan.base.library.common.BaseApplication
+import com.allan.base.library.injection.component.ActivityComponent
+import com.allan.base.library.injection.component.DaggerActivityComponent
+import com.allan.base.library.injection.module.ActivityModule
+import com.allan.base.library.injection.module.LifeCycleProviderModule
+import com.allan.base.library.presenter.BasePresenter
+import com.allan.base.library.presenter.view.BaseView
+import javax.inject.Inject
+
+
+open abstract class BaseMvpFragment<T : BasePresenter<*>> : BaseFragment(), BaseView {
+
+
+    override fun showLoading() {
+
+    }
+
+    override fun hideLoading() {
+
+    }
+
+    override fun onError() {
+
+    }
+
+    @Inject
+    lateinit var mPresenter: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initActivityInjection()
+        initInjection()
+    }
+
+    abstract fun initInjection()
+
+    lateinit var activityComponent: ActivityComponent
+
+    private fun initActivityInjection() {
+        activityComponent =
+            DaggerActivityComponent.builder().appComponent((activity.application as BaseApplication).appComponent)
+                .activityModule(ActivityModule(activity))
+                .lifeCycleProviderModule(LifeCycleProviderModule(this))
+                .build()
+    }
+}
